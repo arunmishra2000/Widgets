2212094c2bd24ae80e3410dcbbfa8ea76ee1e066
arunmishra2000 <arunkumar.mishra2019@vitstudent.ac.in>
2021-07-06 23:57:09 +0530

    XSS

--- src/components/Search.js
+++ src/components/Search.js
@@ -3,15 +3,37 @@ import axios from "axios";
 
 const Search = () => {
   const [term, setTerm] = useState("");
-  console.log("I run with every rerender");
+  const [results, setResults] = useState([]);
+  console.log(results);
   useEffect(() => {
     const search = async () => {
-      await axios.get(
-        "https://en.wikipedia.org/w/api.php?action=query&list=search&format=json&srsearch=programming"
-      );
+      const { data } = await axios.get("https://en.wikipedia.org/w/api.php", {
+        params: {
+          action: "query",
+          list: "search",
+          origin: "*",
+          format: "json",
+          srsearch: term,
+        },
+      });
+      setResults(data.query.search);
     };
-    search();
+    if (term) {
+      search();
+    }
   }, [term]);
+
+  const renderedResults = results.map((result) => {
+    return (
+      <div key={result.pageid} className="item">
+        <div className="right floated content"></div>
+        <div className="content">
+          <div className="header">{result.title}</div>
+          <span dangerouslySetInnerHTML={{ __html: result.snippet }}></span>
+        </div>
+      </div>
+    );
+  });
   return (
     <div>
       <div className="ui form">
@@ -25,6 +47,7 @@ const Search = () => {
           />
         </div>
       </div>
+      <div className="ui celled list">{renderedResults}</div>
     </div>
   );
 };
